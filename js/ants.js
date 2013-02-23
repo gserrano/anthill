07518b515369f73f94ab5d4bb3babcb96df566ef
Guilherme Serrano <guilhermeserrano@asiv.local>
2013-02-23 13:46:57 -0300

    Comments

--- js/ants.js
+++ js/ants.js
@@ -1,5 +1,13 @@
 /*
 	Anthill
+	
+	Anthill is an experimental project to build an "ecosystem" of ants with a bottom-up community.
+	
+	There is no "system rules". Each ant make decisions based on some rules and based on information about 
+	food sources and anthill needs that this ant has in that moment.
+
+	Github - https://github.com/gserrano/anthill
+
 */
 function Anthill(){
 	var _hill = this;
